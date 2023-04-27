eaa37b8e68f272f7bce882ed34bb9be9467fb4e3
DipCoy <dipolarcoyote@gmail.com>
2023-04-27 15:08:11 +0500

    patch directory to move

---
+++ subdirectory_1/sub_file_1.py
@@ -0,0 +1,9 @@
+import math
+
+
+def func():
+    ...
+
+
+if __name__ == '__main__':
+    print(math.cos(0.5))
---
+++ subdirectory_1/sub_file_2.py
@@ -0,0 +1,9 @@
+import math
+
+
+def func():
+    ...
+
+
+if __name__ == '__main__':
+    print(math.sin(0.5))
